0a7c10fdefbf01b6bff9914e4a1fcc7fa4c496bc
Faustito04 <86302670+Faustito04@users.noreply.github.com>
2022-04-11 09:07:52 -0300

    Ejercicio 3b

--- Ejercicios/ejercicio1.js
+++ Ejercicios/ejercicio1.js
@@ -1,59 +1,14 @@
-//CONSIGNA = 11, 33, 2, -1, 110, 99, 8 obtener segundo mayor impar
+var arr = [11, 33, 2, -1, 110, 99, 8];
+PMI = false;
+i = 0;
 
-const http = require('http');
+arr.sort(function(a,b){return a - b;}).reverse();
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-});
-
-let nums = [11, 33, 2, -1, 110, 99, 8]
-
-function calcularImpares() {
-    let impares = []
-
-    for (let a = 0; a < nums.length + 1; a++) {
-        let num = nums[a]
-
-        if (num % 2 != 0) {
-            if (impares.length < 2) {
-                impares[impares.length] = num
-            }
-            else{
-                if (num > impares[0]) {
-                    if(num > impares[1]){
-                        if (impares[1] > impares[0]) {
-                            impares[0] = num
-                        }
-                        else{
-                            impares[1] = num
-                        }
-                    }
-                    else{
-                        impares[0] = num
-                    }
-                }
-                else if(num > impares[1]){
-                    impares[1] = num
-                }
-            }
-            
-        }
+while(!PMI || arr[i]%2==0){
+    if(arr[i]%2!=0){
+        PMI=true
     }
-
-    if (impares[1] > impares[0]) {
-        return impares[0]
-    }
-    else{
-        return impares[1]
-    }
-
+    i++;
 }
 
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-    console.log(calcularImpares())
-});
+console.log(arr[i])
--- Ejercicios/ejercicio3.js
+++ Ejercicios/ejercicio3.js
@@ -1,18 +1,23 @@
 /*
-Duplicar el array anterior y ap´licarle la función raíz a todos los elementos, obteniendo como resultado:
-[{original: 9, modificado: 3}
-]
-*/ 
+Duplicar el array anterior y aplicarle la función raíz a todos los elementos, obteniendo como resultado:
+[{original: 9, modificado: 3}]
+*/
 
-let arrayOriginal = [4, 9, 16, 25, 36, 49, 64, 81, 100, 121];
+let arrayOriginal = [4, -3, 14, 25, 36, 49, 64, 81, 100, 121];
+const result = [];
+
 arrayOriginal = arrayOriginal.concat(arrayOriginal)
-let aux;
 
 for(let i = 0; i < arrayOriginal.length; i++){
-    aux = Math.sqrt(arrayOriginal[i])
-    arrayOriginal[i]={
-        original: arrayOriginal[i],
-        modificado: aux
+    const aux = Math.sqrt(arrayOriginal[i])
+
+    if (!isNaN(aux)) {
+        const rounded = +aux.toFixed(2)
+        result.push({
+            original: arrayOriginal[i],
+            modificado: rounded,
+        });
     }
 }
-console.log(arrayOriginal)
+
+console.log(result)
